4c2cc456ca385ce6794b74de346c7eba124bc0b4
serhii1991 <serhii@kleimovych@gmailcom>
2020-02-23 11:09:55 +0100

    added section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function() {
 
-	
+	$(".header__slider").slick({
+		dots: true,
+		prevArrow: '<button type="button" class="slick-prev"><span class="icon-angle-left"></span></button>',
+		nextArrow: '<button type="button" class="slick-next"><span class="icon-angle-right"></span></button>'
+	});
 
 });
